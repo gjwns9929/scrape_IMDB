a13d9fe6212efc4b0271b9a5e874a5c2b9548a1c
gjwns9929 <86387099+gjwns9929@users.noreply.github.com>
2021-10-25 12:05:25 -0700

    Update imdb_spider.py

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -20,8 +20,11 @@ class ImdbSpider(scrapy.Spider):
 
         Note that the Cast&Crew page has url ending ~ "fullcredits/"
         """
-
+        
+        #add the "fullcredits/" to the start_url for Cast&Crew page.
         cast_crew = response.urljoin("fullcredits/")
+
+        #yield the url, and call the next parse method.
         yield scrapy.Request(cast_crew, callback = self.parse_full_credits)
 
     def parse_full_credits(self,response):
@@ -32,11 +35,14 @@ class ImdbSpider(scrapy.Spider):
 
         Crew members are excluded.
         """
-
+        
+        #shows what to add to the previous url for each actor
         actors_urls = [a.attrib["href"] for a in response.css("td.primary_photo a")]
 
+        #add it to the previous url, and move to each actor's page!
         urls = [response.urljoin(a) for a in actors_urls]
 
+        #yield the url for each actor, and call the last parst method.
         for url in urls:
             yield scrapy.Request(url, callback = self.parse_actor_page)
         
@@ -50,11 +56,13 @@ class ImdbSpider(scrapy.Spider):
         This is a dictionary for each of the movies or TV shows
         in which the actor has worked.
         """
-
+        # extract the name of the actor
         actor_name = response.css("span.itemprop::text")[0].get()
-
+        
+        # extract the list of movies or TV shows for each actor.
         movies_or_TV_shows_names = [a.css("a::text").get() for a in response.css("div.filmo-row")]
 
+        #yield a dictionary for actor name and movies or TV shows.
         for item in movies_or_TV_shows_names:
             yield {
                 "actor" : actor_name,
